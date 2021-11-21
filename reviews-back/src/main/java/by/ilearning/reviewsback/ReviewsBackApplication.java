baa493d0280f4a338c328dcdb72ad60798fc8f21
Andrew Gnevanov <andreyrnd2411@gmail.com>
2021-11-22 00:17:31 +0300

    initial commit

---
+++ reviews-back/src/main/java/by/ilearning/reviewsback/ReviewsBackApplication.java
@@ -0,0 +1,12 @@
+package by.ilearning.reviewsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewsBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewsBackApplication.class, args);
+    }
+}
